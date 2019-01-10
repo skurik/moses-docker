f1fa5ddcfc0e3d8dbf8bf6320d820ff9f37b72f2
Adam Funk <a.funk@sheffield.ac.uk>
2019-01-10 14:57:32 +0000

    Ready for first test

--- train.py
+++ train.py
@@ -44,7 +44,7 @@ oparser.add_argument('-n', dest='dry_run',
 oparser.add_argument('-s', dest='source_language',
                      type=str,
                      required=True,
-                     help='source language (required)')
+                     help='source language')
 
 oparser.add_argument('-t', dest='target_language',
                      default='en', type=str,
@@ -52,7 +52,11 @@ oparser.add_argument('-t', dest='target_language',
 
 oparser.add_argument('-o', dest='output_dir',
                      required=True,
-                     help='model output directory (required)')
+                     help='model output directory')
+
+oparser.add_argument('-u', dest='tuning_base',
+                     required=True,
+                     help='tuning files (without "." and language extension)')
 
 oparser.add_argument('-w', dest='working_dir',
                      default=None,
@@ -83,6 +87,9 @@ truecaser = os.path.join(options.moses_dir, 'scripts', 'recaser', 'truecase.perl
 cleaner = os.path.join(options.moses_dir, 'scripts', 'training', 'clean-corpus-n.perl')
 lmplz = os.path.join(options.moses_dir, 'bin', 'lmplz')
 builder = os.path.join(options.moses_dir, 'bin', 'build_binary')
+trainer = os.path.join(options.moses_dir, 'scripts', 'training', 'train-model.perl')
+
+tools_dir = os.path.join(options.moses_dir, 'tools')
 
 tokenized_source = options.working_file_base + '.tok.' + options.source_language
 tokenized_target = options.working_file_base + '.tok.' + options.target_language
@@ -94,6 +101,8 @@ trued_target = trued_base + '.' + options.target_language
 cleaned_file = options.working_file_base + '.clean'
 arpa_file = options.working_file_base + '.arpa.' + options.target_language
 blm_file = options.working_file_base + '.blm.' + options.target_language
+train_dir = os.path.join(options.output_dir, 'train')
+training_out = os.path.join(options.working_dir, 'training.out')
 
 # /home/moses/mosesdecoder/scripts/tokenizer/tokenizer.perl -l en <news-commentary-v9.ru-en.en >news-commentary-v9.ru-en.tok.en
 # /home/moses/mosesdecoder/scripts/tokenizer/tokenizer.perl -l ru <news-commentary-v9.ru-en.ru >news-commentary-v9.ru-en.tok.ru
@@ -138,29 +147,55 @@ run_command(command, options)
 command = [builder, arpa_file, blm_file]
 run_command(command, options)
 
+# nohup /home/moses/mosesdecoder/scripts/training/train-model.perl --root-dir train
+# -corpus /data/adam/training-nc-v9/news-commentary-v9.ru-en.clean -f ru -e en -alignment grow-diag-final-and
+# -reordering msd-bidirectional-fe -lm 0:3:/data/adam/training-nc-v9/news-commentary-v9.ru-en.blm.en:8
+# -external-bin-dir /home/moses/mosesdecoder/tools >& training.out &
 
+command = [trainer, '--root-dir', train_dir, '-corpus', cleaned_file, '-f', options.source_language,
+           '-e', options.target_language, '-alignment', 'grow-diag-final-and',
+           '-reordering', 'msd-bidirectional-fe', '-m', '0:3:%s:8' % blm_file,
+           '-external-bin-dir', tools_dir, '&>', training_out]
+run_command(command, options)
 
-# mkdir /data/models/working
-#
-# cd /data/models/working
-#
-# nohup /home/moses/mosesdecoder/scripts/training/train-model.perl --root-dir train -corpus /data/adam/training-nc-v9/news-commentary-v9.ru-en.clean -f ru -e en -alignment grow-diag-final-and -reordering msd-bidirectional-fe -lm 0:3:/data/adam/training-nc-v9/news-commentary-v9.ru-en.blm.en:8 -external-bin-dir /home/moses/mosesdecoder/tools >& training.out &
-#
-#
-# cd /data/adam/tuning
-#
+working_tuning_base = os.path.join(options.working_dir, os.path.basename(options.tuning_base))
+tokenized_tuning_source = working_tuning_base + '.tok.' + options.source_language
+tokenized_tuning_target = working_tuning_base + '.tok.' + options.target_language
+trued_tuning_source = working_tuning_base + '.true.' + options.source_language
+trued_tuning_target = working_tuning_base + '.true.' + options.target_language
+
 # /home/moses/mosesdecoder/scripts/tokenizer/tokenizer.perl -l en <newstest2013.en >newstest2013.tok.en
-#
 # /home/moses/mosesdecoder/scripts/tokenizer/tokenizer.perl -l ru <newstest2013.ru >newstest2013.tok.ru
-#
+
+command = [tokenizer, '-l', options.source_language, '<', '%s.%s % (options.tuning_base, options.source_language)'
+           '>', tokenized_tuning_source]
+run_command(command, options)
+
+command = [tokenizer, '-l', options.target_language, '<', '%s.%s % (options.tuning_base, options.target_language)'
+                                                          '>', tokenized_tuning_target]
+run_command(command, options)
+
 # /home/moses/mosesdecoder/scripts/recaser/truecase.perl --model /data/models/truecase-model.en <newstest2013.tok.en >newstest2013.true.en
-#
 # /home/moses/mosesdecoder/scripts/recaser/truecase.perl --model /data/models/truecase-model.ru <newstest2013.tok.ru >newstest2013.true.ru
-#
-# cd /data/models/working
-#
-# nohup /home/moses/mosesdecoder/scripts/training/mert-moses.pl /data/adam/tuning/newstest2013.true.ru /data/adam/tuning/newstest2013.true.en /home/moses/mosesdecoder/bin/moses train/model/moses.ini --mertdir /home/moses/mosesdecoder/bin &>mert.out &
-#
-# cd /data/models
-#
-# /home/moses/mosesdecoder/bin/moses -f mert-work/moses.ini --server --server-log /data/adam/moses-server.log
+
+command = [truecaser, '--model', true_source_model, '<', tokenized_tuning_source,
+           '>', trued_tuning_source]
+run_command(command, options)
+
+command = [truecaser, '--model', true_target_model, '<', tokenized_tuning_target,
+           '>', trued_tuning_target]
+run_command(command, options)
+
+# nohup /home/moses/mosesdecoder/scripts/training/mert-moses.pl /data/adam/tuning/newstest2013.true.ru
+#  /data/adam/tuning/newstest2013.true.en /home/moses/mosesdecoder/bin/moses train/model/moses.ini
+#  --mertdir /home/moses/mosesdecoder/bin &>mert.out &
+
+merter = os.path.join(options.moses_dir, 'scripts', 'training', 'mert-moses.pl')
+moses_bin = os.path.join(options.moses_dir, 'bin', 'moses')
+old_moses_ini = os.path.join(train_dir, 'model', 'moses.ini')
+mert_out = os.path.join(options.working_dir, 'mert.out')
+
+command = [merter, trued_tuning_source, trued_tuning_target, moses_bin, old_moses_ini,
+           '--mertdir', os.path.join(options.moses_dir, 'bin'),
+           '&>', mert_out]
+
