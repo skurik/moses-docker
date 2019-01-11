041f861bbd2426b62911c0da8f26a8ddb6ed94ab
Adam Funk <a.funk@sheffield.ac.uk>
2019-01-11 12:40:55 +0000

    scripts with & without -n

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./train.py -n -s ru -t en -o /data/models/ -w /data/models/working -u /data/training/tuning/newstest2013 -m mosesdecoder/ -i /data/training/training-commoncrawl/commoncrawl.ru-en
+./train.py -s ru -t en -o /data/models/ -w /data/models/working -u /data/training/tuning/newstest2013 -m mosesdecoder/ -i /data/training/training-commoncrawl/commoncrawl.ru-en
