81d2fc40f3a22e01649dd937ef79f535222196ba
Adam Funk <a.funk@sheffield.ac.uk>
2019-04-01 13:22:24 +0100

    moving data

--- download.sh
+++ download.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
+set -e
+
 #  http://www.statmt.org/moses/?n=Moses.LinksToCorpora
 #  "This page is your 'shopping list' for parallel texts."
 
-mkdir -p corpora/training corpora/working
-cd corpora
+mkdir -p /data/corpora/training
+cd /data/corpora
 wget http://www.statmt.org/wmt13/training-parallel-commoncrawl.tgz
 wget http://www.statmt.org/wmt13/dev.tgz
 
 cd training
 tar zxf ../training-parallel-commoncrawl.tgz
 cd ..
-tar zxv dev.tgz
+tar zxf dev.tgz
 # unpacks into dev/*
 mv dev tuning
-
-
